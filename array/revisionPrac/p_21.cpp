e0552c12a72897f0ae12b2e58a90a51e8a05df36
Tanay Vyas <tanayvyas80@gmail.com>
2026-08-30 17:10:59 +0530

    p_21 first last totalOccur

--- array/revisionPrac/p_21.cpp
+++ array/revisionPrac/p_21.cpp
@@ -1,41 +1,77 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
+int firstOccurence(vector<int> &arr, int target)
+{
 
-int firstOccurrenceOfElem(vector<int>&arr , int target){
+    int s = 0;
+    int e = arr.size() - 1;
     int n = arr.size();
-    int s=0;
-    int e=n-1;
-    int ans=-1;
-    while(s<=e)
+    int ans = -1;
+    while (s <= e)
     {
-        int mid = s+(e-s)/2;
+        int mid = s + (e - s) / 2;
 
-        if(arr[mid] == target){
-             ans = mid;
-            e=mid-1;
-        }
-         if(arr[mid]<target)
+        if (arr[mid] == target)
         {
-            s=mid+1;
+            ans = mid;
+            e = mid - 1;
         }
-        else{
-            e=mid-1;
+        if (arr[mid] > target)
+        {
+            e = mid - 1;
         }
-
+        if (arr[mid] < target)
+        {
+            s = mid + 1;
+        }
     }
     return ans;
 }
 
+int lastOccurence(vector<int> &arr, int target)
+{
 
-int main(){
+    int s = 0;
+    int e = arr.size() - 1;
+    int n = arr.size();
+    int ans = -1;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
 
-    vector<int> arr = {10,10,20,20,20,30,40,40,50};
-    int n =arr.size();
-    int target  =10 ;
+        cout << "now s=" << s << " now e=" << e;
+        cout << endl;
 
-    int ans = firstOccurrenceOfElem(arr , target);
+        if (arr[mid] == target)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        if (arr[mid] > target)
+        {
+            e = mid - 1;
+        }
+        if (arr[mid] < target)
+        {
+            s = mid + 1;
+        }
+    }
+    return ans;
+}
 
-    cout<<" index : "<<ans;
+int main()
+{
+
+    vector<int> vec = {10, 20, 20, 20, 30, 40, 40, 50};
+    int target = 50;
+    int firstOccur = firstOccurence(vec, target);
+    int lastOccur = lastOccurence(vec, target);
+
+    int totalOccur = lastOccur - firstOccur + 1;
+
+    cout << "firstOccur : " << firstOccur << endl;
+    cout << "lastOccur : " << lastOccur << endl;
+    cout << "totaltOccur : " << totalOccur << endl;
 }
